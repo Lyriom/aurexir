dbdcd59b6cc12bf5bfdd6ae81246d00c7b2a43bf
Lyriom <jriera@misiva.com.ec>
2026-07-06 19:30:52 -0500

    feat: composición de la landing en App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// Landing de una sola página: header + secciones + footer.
+import AppHeader from './components/AppHeader.vue'
+import HeroSection from './components/HeroSection.vue'
+import CatalogSection from './components/CatalogSection.vue'
+import BrandSection from './components/BrandSection.vue'
+import AppFooter from './components/AppFooter.vue'
+</script>
+
+<template>
+  <AppHeader />
+  <main>
+    <HeroSection />
+    <CatalogSection />
+    <BrandSection />
+  </main>
+  <AppFooter />
+</template>
